fff332b99d24d1b4537990809bab42dbb88ab834
helloqwert12 <tranminhquan1201@gmail.com>
2017-10-13 21:54:10 +0700

    Init project _Part1 - Minh Quan

---
+++ Metroid/dxgraphics.cpp
@@ -0,0 +1,22 @@
+// Direct3D framework source
+
+#include <d3d9.h>
+#include <d3dx9.h>
+#include "dxgraphics.h"
+
+//variable declarations
+LPDIRECT3D9 d3d = NULL;
+LPDIRECT3DDEVICE9 de3ddv = NULL;
+LPDIRECT3DSURFACE9 back_buffer = NULL;
+
+int Init_Direct3D(HWND hWnd, int width, int height, int fullscreen)
+{
+	//TO-DO: Update soon
+	return 0;
+}
+
+LPDIRECT3DSURFACE9 LoadSurface(char* filename, D3DCOLOR transcolor)
+{
+	//TO-DO: Update soon
+}
+
