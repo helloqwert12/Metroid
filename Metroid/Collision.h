a636ad06c644a15798b492fa74aff879a223f16f
Quoc khang Luong <khanghuu97@gmail.com>
2017-11-13 14:50:19 +0700

    cập nhật hàm isColling , sweptAABB

---
+++ Metroid/Collision.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "GameObject.h"
+
+class Collision
+{
+public:
+	Collision();
+	~Collision();
+
+	static bool isColliding(GameObject objectA , GameObject objectB);
+	static float sweptAABB(GameObject objectA, GameObject objectB, float& normalx, float& normaly);
+};
+
