d1d4e7fddec10d77f070a02ed6d7757e057da59b
Kiet Vo <geforce2409@gmail.com>
2017-11-15 22:27:45 +0700

    Revert "dsa"
    This reverts commit e862c3f49811ee6890a244da7ad3031213253c9d.

--- Metroid/Metroid.cpp
+++ Metroid/Metroid.cpp
@@ -1,9 +1,6 @@
 #include "Metroid.h"
 #include <time.h>
 #include "trace.h"
-#include "utils.h"
-
-#define BACKGROUND_FILE L"background\\temp_background.png"
 
 void Metroid::_InitBackground()
 {
@@ -33,14 +30,6 @@ Metroid::~Metroid()
 
 void Metroid::RenderFrame(LPDIRECT3DDEVICE9 d3ddv, int Delta)
 {
-	// Background
-	d3ddv->StretchRect(
-		Background,			// from 
-		NULL,				// which portion?
-		_BackBuffer,		// to 
-		NULL,				// which portion?
-		D3DTEXF_NONE);
-
 	samus->Update(Delta);
 }
 
@@ -136,15 +125,6 @@ void Metroid::LoadResources(LPDIRECT3DDEVICE9 d3ddv)
 	srand((unsigned)time(NULL));
 	_InitSprites(d3ddv);
 	_InitPositions();
-
-	Background = CreateSurfaceFromFile(_d3ddv, BACKGROUND_FILE);
-
-	samus->SetPosX(50);
-	samus->SetPosY(200);
-
-	samus->SetVelocityX(0);
-	samus->SetVelocityXLast(1.0f);
-	samus->SetPosY(0);
 }
 
 void Metroid::OnKeyDown(int KeyCode)
@@ -214,6 +194,5 @@ void Metroid::OnKeyDown(int KeyCode)
 			samus->SetState(RIGHTING);
 		break;
 	}
-
 
 }
--- Metroid/main.cpp
+++ Metroid/main.cpp
@@ -4,7 +4,7 @@
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	Metroid metroid(hInstance, L"Metroid game", GAME_SCREEN_RESOLUTION_640_480_24, 0, 24);
+	Metroid metroid(hInstance, L"Metroid game", GAME_SCREEN_RESOLUTION_800_600_24, 0, 24);
 
 	metroid.Init();
 	metroid.Run();
